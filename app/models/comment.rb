bd96bf883a41d4ac374bf55ff11fc88994e48c5f
epascual2014 <Edrickpascual@yahoo.com>
2014-03-17 18:02:19 -0700

    Ajaxified comment destruction

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -7,7 +7,7 @@ class Comment < ActiveRecord::Base
 
   def send_favorites_emails
     # every favorite assoc w/ post send email
-    self.post.favorites.each do |favorites| 
+    self.post.favorites.each do |favorite| 
       if favorite.user_id != self.user_id && favorite.user.email_favorites?
         FavoriteMailer.new_comment(favorite.user, self.post, self).deliver
       end
